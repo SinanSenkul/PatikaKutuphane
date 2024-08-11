7feef2a8f8fe17aa5fed916f9aaefc3ca3464cf1
sinan senkul <sinan@sinan-senkul.local>
2024-08-11 18:41:21 +0300

    created book object

---
+++ PatikaKutuphane/Book.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+public class Book
+{
+    // Field
+    public string Name { get; set; }
+    public string WriterName { get; set; }
+    public string WriterLastName { get; set; }
+    public int PageNum { get; set; }
+    public string Publisher { get; set; }
+    public DateTime PubDate { get; set; }
+
+    // Constructors
+    public Book()
+    {
+        Name = "Adı Aylin";
+        WriterName = "Ayşe";
+        WriterLastName = "Kulin";
+        PageNum = 398;
+        Publisher = "Remzi Kitabevi";
+        PubDate = DateTime.Now;
+    }
+
+    public Book(string name, string writerName, string writerLastName, int pageNum, string publisher)
+    {
+        Name = name;
+        WriterName = writerName;
+        WriterLastName = writerLastName;
+        PageNum = pageNum;
+        Publisher = publisher;
+        PubDate = DateTime.Now;
+    }
+}
